41fef51264e10275721d0ea858250d1190fac743
joaovitor774 <joaovitordavy76@gmail.com>
2026-03-27 19:36:40 -0400

    Create V2__insert_into_contrato.sql

---
+++ scripts/V2__insert_into_contrato.sql
@@ -0,0 +1,4 @@
+INSERT INTO contrato (cliente_id, veiculo_id, data_inicio, data_fim, valor_total, tipo_pagamento)
+VALUES
+(1,1,'2026-03-01','2026-03-10',1500,'PIX'),
+(2,2,'2026-03-05','2026-03-15',800,'CARTAO');
